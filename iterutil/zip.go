173b6924cec485b48b750d4cb2a7928f1f853eaa
eNV25 <env252525@gmail.com>
2024-12-05 02:59:51 +0100

    refactor(iterutil): improve generic constraints

--- iterutil/zip.go
+++ iterutil/zip.go
@@ -4,10 +4,10 @@ import (
 	"iter"
 )
 
-func Zip[T, U any](t []T, u []U) iter.Seq2[T, U] {
-	return func(yield func(T, U) bool) {
-		for i := range min(len(t), len(u)) {
-			if !yield(t[i], u[i]) {
+func Zip[S1 ~[]E1, S2 ~[]E2, E1 any, E2 any](s1 S1, s2 S2) iter.Seq2[E1, E2] {
+	return func(yield func(E1, E2) bool) {
+		for i := range min(len(s1), len(s2)) {
+			if !yield(s1[i], s2[i]) {
 				return
 			}
 		}
