1d91207b5713a9c8f73e18afc37ebf55ddc17f7c
eNV25 <env252525@gmail.com>
2024-12-07 20:03:44 +0100

    add setutil

---
+++ setutil/set.go
@@ -0,0 +1,38 @@
+package setutil
+
+type (
+	empty = struct{}
+
+	// TODO: go1.24: use alias
+	// set[T comparable] = map[T]empty
+	Set[T comparable] map[T]empty
+)
+
+func Contains[S ~map[E]empty, E comparable](s S, v E) bool {
+	_, found := s[v]
+	return found
+}
+
+func Add[S ~map[E]empty, E comparable](s S, vs ...E) {
+	for _, v := range vs {
+		s[v] = empty{}
+	}
+}
+
+func Delete[S ~map[E]empty, E comparable](s S, vs ...E) {
+	for _, v := range vs {
+		delete(s, v)
+	}
+}
+
+func (s Set[T]) Contains(v T) bool {
+	return Contains(s, v)
+}
+
+func (s Set[T]) Add(vs ...T) {
+	Add(s, vs...)
+}
+
+func (s Set[T]) Delete(vs ...T) {
+	Delete(s, vs...)
+}
